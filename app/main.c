860c8ee9e8a6d4900f703241e88618c8600ff043
mrtea9 <ionmeriacri111@gmail.com>
2024-10-11 16:58:44 +0300

    parse torrent file

--- app/main.c
+++ app/main.c
@@ -298,7 +298,7 @@ int process_command(char* command, char* encoded_str) {
         
         size_t bytesRead = 0;
         unsigned char* file_content = read_file(encoded_str, &bytesRead);
-        value* result = decode_bencode(hex_dump_to_char(file_content, bytesRead)));
+        value* result = decode_bencode(hex_dump_to_char(file_content, bytesRead));
         value_println(result);
         value_delete(result);
 
