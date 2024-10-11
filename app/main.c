1e6bc1237624a01446c0aa574f5d64068e0faa78
mrtea9 <ionmeriacri111@gmail.com>
2024-10-11 15:49:57 +0300

    parse torrent file

--- app/main.c
+++ app/main.c
@@ -258,6 +258,28 @@ unsigned char* read_file(const char* filename, size_t* length) {
     return buffer;
 }
 
+void print_hex_dump(const unsigned char* buffer, size_t length) {
+    printf("Hex dump of file contents:\n");
+    for (size_t i = 0; i < length; i++) {
+        // Print byte in hex format
+        printf("%02x ", buffer[i]);
+
+        // Print ASCII representation if printable, otherwise a dot
+        if (isprint(buffer[i])) {
+            printf("(%c) ", buffer[i]);
+        }
+        else {
+            printf("(.) ");
+        }
+
+        // Print a new line every 16 bytes for readability
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
 int process_command(char* command, char* encoded_str) {
     if (strcmp(command, "decode") == 0) {
         value* result = decode_bencode(encoded_str);
@@ -270,7 +292,7 @@ int process_command(char* command, char* encoded_str) {
         unsigned char* file_content = read_file(encoded_str, &file_length);
 
         printf("daa\n");
-        printf("%s\n", file_content);
+        print_hex_dump(file_content, file_length);
 
         exit(1);
     }
