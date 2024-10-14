1a50579e1f6f8c4be9298930451c12333f676f6f
mrtea9 <ionmeriacri111@gmail.com>
2024-10-14 10:49:17 +0300

    parse torrent file

--- app/main.c
+++ app/main.c
@@ -201,10 +201,7 @@ value* value_get(value* val, char* name) {
 
     if (val->type != VAL_DICT) exit(1);
 
-    for (int i = 0; i < val->count; i += 2) {
-        if (val->cell[i]->type == VAL_DICT) {
-            value_get(val, name);
-        }
+    for (int i = 0; i < val->count; i++) {
 
         value_print(val->cell[i]);
         putchar(' ');
