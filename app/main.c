7ed84a551fdd99901921f2e41edbd3966be282c6
mrtea9 <ionmeriacri111@gmail.com>
2024-10-16 17:18:17 +0300

    Calculate info hash

--- app/main.c
+++ app/main.c
@@ -350,7 +350,7 @@ char* encode(value* decoded) {
 }
 
 char* calculate_hash(value* value) {
-    char* hash_string[SHA_DIGEST_LENGTH * 2 + 1];
+    char hash_string[SHA_DIGEST_LENGTH * 2 + 1];
     char* encoded_value = encode(value);
     size_t length = strlen(encoded_value);
 
