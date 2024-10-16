248b45c4f4e8c5fb7a02310e16a1b5b91559cc6e
mrtea9 <ionmeriacri111@gmail.com>
2024-10-16 16:17:33 +0300

    Calculate info hash

--- app/main.c
+++ app/main.c
@@ -436,7 +436,7 @@ int process_command(char* command, char* encoded_str) {
         value* info = value_get(result, "info");
 
         value_println(info);
-        printf("%s\n", encode(result));
+        printf("%s\n", encode(info));
         //value_println(result);
         //value_println(info);
         
