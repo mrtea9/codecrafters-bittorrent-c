013efbb1cc11d7cb34c84e2fbdc972746e1c5e7e
mrtea9 <ionmeriacri111@gmail.com>
2024-10-16 10:31:49 +0300

    Calculate info hash

--- app/main.c
+++ app/main.c
@@ -382,7 +382,7 @@ int process_command(char* command, char* encoded_str) {
         
         //value_println(result);
         //value_println(info);
-        printf("%s\n", encoded_str);
+        printf("%s\n", file_content);
 
         //value_print_info(announce);
         //putchar('\n');
