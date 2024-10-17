ce485e1b24039b39eaa721e5310d5fd8b84cac1b
mrtea9 <ionmeriacri111@gmail.com>
2024-10-17 14:11:35 +0300

    Added load, print and error

--- app/main.c
+++ app/main.c
@@ -451,7 +451,17 @@ int process_command(char* command, char* encoded_str) {
         value_delete(info);
     }
     else if (strcmp(command, "peers") == 0) {
-        printf("%s\n", encoded_str);
+        size_t bytesRead = 0;
+        unsigned char* file_content = read_file(encoded_str, &bytesRead);
+
+        value* result = decode_bencode(file_content);
+        value* announce = value_get(result, "announce");
+        value* length = value_get(result, "length");
+        value* info = value_get(result, "info");
+        value* piece_length = value_get(result, "piece length");
+        value* pieces = value_get(result, "pieces");
+
+        value_println(result);
     }
     else {
         fprintf(stderr, "Unknown command: %s\n", command);
