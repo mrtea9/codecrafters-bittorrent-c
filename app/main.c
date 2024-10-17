e59fd963c940b1f2399a1907d80bc70bba7771db
mrtea9 <ionmeriacri111@gmail.com>
2024-10-17 11:00:19 +0300

    Added load, print and error

--- app/main.c
+++ app/main.c
@@ -466,7 +466,7 @@ int process_command(char* command, char* encoded_str) {
         printf("finded info = %s\n", finded_info);
         printf("encoded info = %s\n", encoded_info);
         printf("Info Hash: %s\n", calculate_hash(encoded_info, strlen(encoded_info)));
-        printf("Info Hash: %s", calculate_hash(finded_info, 24));
+        printf("Info Hash: %s", calculate_hash(finded_info, 112));
 
         value_delete(result);
         value_delete(announce);
