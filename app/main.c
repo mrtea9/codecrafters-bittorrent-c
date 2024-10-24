e932e1fc5611111662988c5f9d37b523e558c19f
mrtea9 <ionmeriacri111@gmail.com>
2024-10-24 16:15:26 +0300

    Download a piece

--- app/main.c
+++ app/main.c
@@ -656,7 +656,7 @@ void send_handshake(int sockfd, value* result) {
     unsigned char* raw_info_hash = calculate_raw_hash(encoded_info, strlen(encoded_info));
     unsigned char handshake[HANDSHAKE_LEN];
     char peer_id[] = "23141516167152146123";
-
+    printf("da");
     handshake[0] = PROTOCOL_LEN;
     memcpy(&handshake[1], "BitTorrent protocol", PROTOCOL_LEN);
     memset(&handshake[20], 0, RESERVED_LEN);
@@ -800,7 +800,6 @@ int peer_handshake(char* encoded_str, char* address) {
         close(sockfd);
         exit(1);
     }
-
 
 
     send_handshake(sockfd, result);
